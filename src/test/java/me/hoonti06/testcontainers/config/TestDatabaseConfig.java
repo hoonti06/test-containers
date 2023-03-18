45b2f7ca31e40e42b37f626280ee9c8cee52bd09
hoonti06 <hoonti06@gmail.com>
2023-03-18 19:17:32 +0900

    feat : TestContainer testconfiguration 및 MemberRepository 테스트 추가
    MemberRepository.save() 호출 시 에러 발생 안하지만 실제로는 저장 안 됨

---
+++ src/test/java/me/hoonti06/testcontainers/config/TestDatabaseConfig.java
@@ -0,0 +1,52 @@
+package me.hoonti06.testcontainers.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@Profile("test")
+@TestConfiguration
+public class TestDatabaseConfig {
+
+  @Bean
+  public MariaDBContainer mariaDbContainer() {
+    final MariaDBContainer mariaDbContainer = new MariaDBContainer(DockerImageName.parse("mariadb:10.6"));
+    mariaDbContainer.start();
+    return mariaDbContainer;
+  }
+
+  @Bean
+  @DependsOn("mariaDbContainer")
+  public HikariDataSource dataSource(MariaDBContainer mariaDbContainer) {
+    HikariConfig config = new HikariConfig();
+    config.setJdbcUrl(mariaDbContainer.getJdbcUrl());
+    config.setUsername(mariaDbContainer.getUsername());
+    config.setPassword(mariaDbContainer.getPassword());
+    HikariDataSource dataSource = new HikariDataSource(config);
+    initSchema(dataSource);
+    return dataSource;
+  }
+
+  private void initSchema(DataSource dataSource) {
+    Flyway flyway = Flyway
+        .configure()
+        .dataSource(dataSource)
+        .load();
+    flyway.migrate();
+  }
+
+  @Bean
+  public PlatformTransactionManager transactionManager(DataSource dataSource) {
+    return new DataSourceTransactionManager(dataSource);
+  }
+
+}
---
+++ src/test/java/me/hoonti06/testcontainers/repository/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package me.hoonti06.testcontainers.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lombok.extern.slf4j.Slf4j;
+import me.hoonti06.testcontainers.config.TestDatabaseConfig;
+import me.hoonti06.testcontainers.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+@Slf4j
+@ActiveProfiles("test")
+@DataJpaTest
+@Import(TestDatabaseConfig.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class MemberRepositoryTest {
+
+  @Autowired private MemberRepository memberRepository;
+
+  @Test
+  void save() {
+    // given
+    Member member = new Member();
+    member.setName("hoon");
+    member.setAddress("Seoul");
+
+    // when
+    memberRepository.save(member);
+
+    // then
+    final Member findMember = memberRepository.findById(member.getId()).get();
+    assertThat(findMember.getId()).isEqualTo(member.getId());
+  }
+
+}
