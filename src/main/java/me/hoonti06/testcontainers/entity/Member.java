92f3fea5459772d0e160b2b1f797b51122449c82
hoonti06 <hoonti06@gmail.com>
2023-03-18 22:15:42 +0900

    fix : 테이블명을 member로 명시 선언

--- src/main/java/me/hoonti06/testcontainers/entity/Member.java
+++ src/main/java/me/hoonti06/testcontainers/entity/Member.java
@@ -4,10 +4,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Table(name = "member")
 @Getter
 @Setter
 public class Member {
