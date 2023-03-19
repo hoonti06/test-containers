33dbdd2ac4bbbb2bd04260e97c6053f15d644637
hoonti06 <hoonti06@gmail.com>
2023-03-19 09:39:26 +0900

    test : 데이터베이스 테이블 초기화 코드 추가

--- src/test/java/me/hoonti06/testcontainers/repository/MemberRepositoryTest.java
+++ src/test/java/me/hoonti06/testcontainers/repository/MemberRepositoryTest.java
@@ -3,8 +3,10 @@ package me.hoonti06.testcontainers.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import lombok.extern.slf4j.Slf4j;
+import me.hoonti06.testcontainers.DatabaseCleanup;
 import me.hoonti06.testcontainers.config.TestDatabaseConfig;
 import me.hoonti06.testcontainers.entity.Member;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -15,11 +17,17 @@ import org.springframework.test.context.ActiveProfiles;
 @Slf4j
 @ActiveProfiles("test")
 @DataJpaTest
-@Import(TestDatabaseConfig.class)
+@Import({TestDatabaseConfig.class, DatabaseCleanup.class})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class MemberRepositoryTest {
 
   @Autowired private MemberRepository memberRepository;
+  @Autowired private DatabaseCleanup databaseCleanup;
+
+  @BeforeEach
+  void setUp() {
+    databaseCleanup.execute();
+  }
 
   @Test
   void save() {
@@ -27,6 +35,21 @@ class MemberRepositoryTest {
     Member member = new Member();
     member.setName("hoon");
     member.setAddress("Seoul");
+
+    // when
+    memberRepository.save(member);
+
+    // then
+    final Member findMember = memberRepository.findById(member.getId()).get();
+    assertThat(findMember.getId()).isEqualTo(member.getId());
+  }
+
+  @Test
+  void save2() {
+    // given
+    Member member = new Member();
+    member.setName("kim");
+    member.setAddress("Jeju");
 
     // when
     memberRepository.save(member);
