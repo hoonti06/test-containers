162020b48965da057b9ea281e825f581f7b6af44
hoonti06 <hoonti06@gmail.com>
2023-03-25 20:03:34 +0900

    test : application test에 test profile 적용

--- src/test/java/me/hoonti06/testcontainers/TestContainerApplicationTests.java
+++ src/test/java/me/hoonti06/testcontainers/TestContainerApplicationTests.java
@@ -1,8 +1,11 @@
 package me.hoonti06.testcontainers;
 
+import me.hoonti06.testcontainers.config.TestDatabaseConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@Import(TestDatabaseConfig.class)
 @SpringBootTest
 class TestContainerApplicationTests {
 
